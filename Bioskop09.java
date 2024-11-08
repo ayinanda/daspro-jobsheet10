b5ffb417731e94cd432dd0adbd97f7fed2a11af6
ayinanda <ayiniswati22@gmail.com>
2024-11-08 14:31:36 +0700

    Bioskop09

---
+++ Bioskop09.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Bioskop09 {
+
+    public static void main(String[] args) {
+        Scanner sc09 = new Scanner(System.in);
+
+        String [][] penonton = new String[4][2];
+
+        penonton[0][0] = "Amin";
+        penonton[0][1] = "Bena";
+        penonton[1][0] = "Candra";
+        penonton[1][1] = "Dela";
+        penonton[2][0] = "Eka";
+        penonton[2][1] = "Farhan";
+        penonton[3][0] = "Gisel";
+        penonton[3][1] = "Hana";
+
+        for (int i = 0; i<penonton.length; i++) {
+            System.out.println(" Penonton pada baris ke- " + (i+1) + ": " + String.join(",", penonton[i]));
+        }
+    }
+}
